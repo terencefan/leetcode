ed0c1334a0c78b73307218a9878c80cbb1fea837
stdrickforce <stdrickforce@gmail.com>
2017-03-17 15:57:38 +0800

    use heap do this.

--- python/313.py
+++ python/313.py
@@ -4,6 +4,8 @@
 # Author: stdrickforce (Tengyuan Fan)
 # Email: <stdrickforce@gmail.com> <fantengyuan@baixing.com>
 
+import heapq
+
 
 class Solution(object):
     def nthSuperUglyNumber(self, n, primes):
@@ -12,16 +14,19 @@ class Solution(object):
         :type primes: List[int]
         :rtype: int
         """
-        r, index = [1], [0] * len(primes)
-        for i in range(1, n):
-            r.append(r[index[0]] * primes[0])
-            ks = [0]
-            for j in range(1, len(primes)):
-                m = r[index[j]] * primes[j]
-                if m < r[i]:
-                    ks, r[i] = [j], m
-                elif m == r[i]:
-                    ks.append(j)
-            for k in ks:
-                index[k] += 1
+        r, index = [1], []
+        for p in primes:
+            heapq.heappush(index, (p, p, 0))
+
+        i = 1
+        while i < n:
+            x, p, j = heapq.heappop(index)
+            if x > r[-1]:
+                r.append(x)
+                i += 1
+            heapq.heappush(index, (p * r[j + 1], p, j + 1))
         return r[n - 1]
+
+
+x = Solution().nthSuperUglyNumber(25, [2, 3, 5])
+print(x)
